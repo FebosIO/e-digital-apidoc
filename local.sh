075895b47fc1b68cfd45c3ca020f63b84042e3f5
Michel Muñoz <michel@febos.cl>
2020-07-31 18:39:41 -0400

    ajuste script

--- local.sh
+++ local.sh
@@ -1,9 +1,9 @@
 ###!/bin/bash
 rm sitio/api.spec.yaml
-rm sitio/api.pre-spec.yaml 
+rm sitio/api.spec.yaml.bak
+rm sitio/api.pre-spec.yaml
 swagger-cli bundle -t yaml -o sitio/api.pre-spec.yaml api.cl.yaml
 openapi-snippet sitio/api.pre-spec.yaml -t shell_curl -t java_okhttp -t csharp -t javascript -t node -t php -t python -o sitio/api.spec.yaml
-rm sitio/api.pre-spec.yaml
 sed -i.bak "s/x-codeSamples/x-code-samples/g" sitio/api.spec.yaml
 echo "http://localhost:1234"
 php -S localhost:1234 -t sitio
